5bbc65ca67d21d38372a65926416d1116054353c
Diego <diegoaragao465@gmail.com>
2023-05-20 10:29:25 -0300

    Support for orama

--- Data.py
+++ Data.py
@@ -13,7 +13,7 @@ jsonConfig = open("config.json")
 config = json.load(jsonConfig)
 
 # connect to MetaTrader 5
-if not mt5.initialize(login=config["login"], server="MetaQuotes-Demo", password=config["password"]):
+if not mt5.initialize(login=config["login"], server="OramaDTVM-Server", password=config["password"]):
     print("initialize() failed")
     mt5.shutdown()
 
@@ -21,7 +21,7 @@ timezone = pytz.timezone("UTC")
 # create 'datetime' object in UTC time zone to avoid the implementation of a local time zone offset
 utc_from = datetime(2020, 1, 10, tzinfo=timezone)
 # get 10 EURUSD H4 bars starting from 01.10.2020 in UTC time zone - TIMEFRAME_H4 = each 4 hours
-rates = mt5.copy_rates_from("EURUSD", mt5.TIMEFRAME_H4, utc_from, 10)
+rates = mt5.copy_rates_from("PETR4", mt5.TIMEFRAME_H4, utc_from, 10)
 
 # shut down connection to the MetaTrader 5 terminal
 mt5.shutdown()
