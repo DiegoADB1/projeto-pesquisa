c5bc413dcdf659ec5a9b023889e366a16040bef7
Diego <diegoaragao465@gmail.com>
2023-05-27 11:19:36 -0300

    Add range datetime

--- Data.py
+++ Data.py
@@ -19,9 +19,12 @@ if not mt5.initialize(login=config["login"], server="OramaDTVM-Server", password
 
 timezone = pytz.timezone("UTC")
 # create 'datetime' object in UTC time zone to avoid the implementation of a local time zone offset
-utc_from = datetime(2020, 1, 10, tzinfo=timezone)
-# get 10 EURUSD H4 bars starting from 01.10.2020 in UTC time zone - TIMEFRAME_H4 = each 4 hours
-rates = mt5.copy_rates_from("PETR4", mt5.TIMEFRAME_D1, utc_from, 5000)
+# YYYY-MM-DD
+utc_from = datetime(2021, 5, 27, tzinfo=timezone)
+utc_to = datetime(2023, 5, 27, tzinfo=timezone)
+
+# get 10 EURUSD H4 bars starting from 01.10.2020 in UTC time zone - TIMEFRAME_D1 = each 1 day
+rates = mt5.copy_rates_range("PETR4", mt5.TIMEFRAME_D1, utc_from, utc_to)
 
 # shut down connection to the MetaTrader 5 terminal
 mt5.shutdown()
